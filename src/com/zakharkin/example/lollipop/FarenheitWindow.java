683b5800eeaa37c9bf6b57271ea4ea4ce95e5ef2
Andrew Zakharkin <zakharking@gmail.com>
2014-11-22 18:33:20 +0300

    first

---
+++ src/com/zakharkin/example/lollipop/FarenheitWindow.java
@@ -0,0 +1,15 @@
+package com.zakharkin.example.lollipop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FarenheitWindow extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

21ecaa6f8be88f747aa4f41b5d7dd23fe1b9543b
Andrew Zakharkin <zakharking@gmail.com>
2015-01-12 21:23:04 +0300

    Mm Hg and PSI

--- src/com/zakharkin/example/lollipop/FahrenheitWindow.java
+++ src/com/zakharkin/example/lollipop/FahrenheitWindow.java
@@ -8,8 +8,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class FahrenheitWindow extends Activity {
-    private TemperatureTranslator translator = new TemperatureTranslator();
+    private GeneralTranslator translator = new GeneralTranslator();
     Button buttonCelsius, buttonFahrenheit;
+    Button buttonMmHg, buttonPsi;
     EditText editTextFrom;
     TextView resultText, textViewResult;
 
@@ -19,6 +20,8 @@ public class FahrenheitWindow extends Activity {
         setContentView(R.layout.main);
         buttonCelsius = (Button) findViewById(R.id.buttonCelsius);
         buttonFahrenheit = (Button) findViewById(R.id.buttonFahrenheit);
+        buttonMmHg = (Button) findViewById(R.id.buttonMmHg);
+        buttonPsi = (Button) findViewById(R.id.buttonPsi);
         editTextFrom = (EditText) findViewById(R.id.editTextFrom);
         textViewResult = (TextView) findViewById(R.id.textViewResultValue);
         resultText = (TextView) findViewById(R.id.textViewResult);
@@ -38,6 +41,22 @@ public class FahrenheitWindow extends Activity {
                 resultText.setText(buttonFahrenheit.getText());
             }
         });
+
+        buttonMmHg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                pressTranslate(true);
+                resultText.setText(buttonMmHg.getText());
+            }
+        });
+
+        buttonPsi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                pressTranslate(false);
+                resultText.setText(buttonPsi.getText());
+            }
+        });
     }
 
     private void translate(boolean toCelsius) {
@@ -50,5 +69,17 @@ public class FahrenheitWindow extends Activity {
         }
         editTextFrom.setText(Double.toString(from));
         textViewResult.setText(Double.toString(toCelsius ? translator.ToCelsius(from) : translator.ToFahrenheit(from)));
+    }
+
+    private void pressTranslate(boolean toMmHg){
+        double from;
+        try{
+            from = Double.parseDouble(editTextFrom.getText().toString());
+        }
+        catch (Exception e){
+            from = 0.0;
+        }
+        editTextFrom.setText(Double.toString(from));
+        textViewResult.setText(Double.toString(toMmHg ? translator.ToMmHg(from) : translator.ToPsi(from)));
     }
 }
--- src/com/zakharkin/example/lollipop/GeneralTranslator.java
+++ src/com/zakharkin/example/lollipop/GeneralTranslator.java
@@ -3,12 +3,20 @@ package com.zakharkin.example.lollipop;
 /**
  * Created by Andrew on 23.11.2014.
  */
-public class TemperatureTranslator {
+public class GeneralTranslator {
     public double ToFahrenheit(double from){
         return Math.round(from * 90000.0 / 5.0 + 320000.0) / 10000.0;
     }
 
     public double ToCelsius(double from){
         return Math.round((from - 32.0) * 50000.0 / 9.0) / 10000.0;
+    }
+
+    public double ToMmHg(double from){
+        return Math.round(from * 10000.0 / 0.019336721269668) / 10000.0;
+    }
+
+    public double ToPsi(double from){
+        return Math.round(from * 193.36721269668) / 10000.0;
     }
 }
